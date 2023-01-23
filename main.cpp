93b643144265e95b9920730a80bee5fbde9adf80
Nathan Guillermo <nathan.glrm@gmail.com>
2023-01-23 23:26:14 +0100

    Add commentary

--- main.cpp
+++ main.cpp
@@ -5,12 +5,21 @@
 #include "src/Map/Map.h"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(1200, 1000), "My window");
-    MapView map = MapView(&window);
 
+	//Create SFML Window object
+	sf::RenderWindow window(sf::VideoMode(1200, 1000), "My window");
+    //Create custom View to handle all camera movement
+	MapView map = MapView(&window);
+
+	//Create a in-simulation Map
     Map m("./assets/MapData/node.txt", "./assets/MapData/road.txt");
 
+	//Render loop
     while (window.isOpen()){
+
+		//
+		//Treatment of events
+		//
         sf::Event event{};
         while (window.pollEvent(event))
         {
@@ -18,8 +27,14 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+		//Move camera if needed
 		map.moveLoops();
 
+
+		//		 //
+		//Drawing//
+		//		 //
         window.clear(sf::Color::Black);
         window.setView(map.getView());
         m.draw(window);
